44beb916356259f6c58eab137f164ae961f7d8fa
Benjamin Ghis <benjamin.ghis@avid.com>
2020-06-09 20:05:47 -0700

    adding filecatalyst

---
+++ MediaComposer-BYOL/scripts/installFilecatalyst.ps1
@@ -0,0 +1,89 @@
+<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
+param (
+    [ValidateNotNullOrEmpty()]
+    $FileCatalystMediaShuttleURL,
+    [ValidateNotNullOrEmpty()]
+    $AvidNEXISClientURL
+)
+
+filter Timestamp {"$(Get-Date -Format o): $_"}
+
+function
+Write-Log($message) {
+    $msg = $message | Timestamp
+    Write-Output $msg
+}
+
+function
+DownloadFileOverHttp($Url, $DestinationPath) {
+    $secureProtocols = @()
+    $insecureProtocols = @([System.Net.SecurityProtocolType]::SystemDefault, [System.Net.SecurityProtocolType]::Ssl3)
+
+    foreach ($protocol in [System.Enum]::GetValues([System.Net.SecurityProtocolType])) {
+        if ($insecureProtocols -notcontains $protocol) {
+            $secureProtocols += $protocol
+        }
+    }
+    [System.Net.ServicePointManager]::SecurityProtocol = $secureProtocols
+
+    # make Invoke-WebRequest go fast: https://stackoverflow.com/questions/14202054/why-is-this-powershell-code-invoke-webrequest-getelementsbytagname-so-incred
+    $ProgressPreference = "SilentlyContinue"
+    Invoke-WebRequest $Url -UseBasicParsing -OutFile $DestinationPath -Verbose
+    Write-Log "$DestinationPath updated"
+}
+
+function 
+Install-NexisClient {
+   
+    Write-Log "downloading Nexus Client"
+    $NexisDestinationPath = "D:\AzureData\AvidNEXISClient.msi"
+    Write-Log $DestinationPath
+    DownloadFileOverHttp $AvidNEXISClientURL $NexisDestinationPath
+
+    Start-Process -FilePath $NexisDestinationPath -ArgumentList "/quiet", "/passive", "/norestart" -Wait
+    
+}
+
+function
+Install-ChocolatyAndPackages {
+    
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+    Write-Log "choco Install Google Chrome"
+    choco install -y googlechrome -ignore-checksum
+
+}
+
+function 
+Install-FileCatalyst {
+   
+    Write-Log "downloading FileCatalyst Server"
+    $FileCatalystDestinationPath = "C:\Users\Public\Desktop\Install_FileCatalyst.exe"
+
+    Write-Log $FileCatalystDestinationPath
+    DownloadFileOverHttp $FileCatalystMediaShuttleURL $FileCatalystDestinationPath
+
+}
+
+try {
+    $dest = "D:\AzureData"
+    New-Item -Path $dest -ItemType directory -Force
+
+    try {
+            Write-Log "Installing chocolaty and packages"
+            Install-ChocolatyAndPackages
+        }
+        catch {
+            # chocolaty is best effort
+        }
+    
+    Write-Log "Call Install-NexisCLient"
+    Install-NexisClient
+
+    Write-Log "Call Install-SFileCatalyst"
+    Install-FileCatalyst
+}
+catch {
+    Write-Error $_
+}
+
