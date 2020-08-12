535a984b2bd3e7522cc9413bb3d5b5cb33ad55ef
Benjamin Ghis <benjamin.ghis@avid.com>
2020-08-12 12:24:53 -0700

    remove Teradici agent licensing

--- MediaComposer-BYOL/scripts/clone-post-config.ps1
+++ MediaComposer-BYOL/scripts/clone-post-config.ps1
@@ -1,6 +1,4 @@
-param (
-    [Parameter(Mandatory=$true, ParameterSetName="Register")][string]$RegistrationCode
-)
+
 Start-Sleep 10
 rename-computer (Invoke-RestMethod -Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
 Start-Sleep 5
