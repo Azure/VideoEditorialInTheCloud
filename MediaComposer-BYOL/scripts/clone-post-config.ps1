7f33b5f343c466059debac08f10bcbf4e6934ad8
Benjamin Ghis <benjamin.ghis@avid.com>
2020-08-12 13:57:33 -0700

    add teradici licensing back

--- MediaComposer-BYOL/scripts/clone-post-config.ps1
+++ MediaComposer-BYOL/scripts/clone-post-config.ps1
@@ -1,5 +1,8 @@
-
+param (
+    [Parameter(Mandatory=$true, ParameterSetName="Register")][string]$RegistrationCode
+)
 Start-Sleep 10
-rename-computer (Invoke-RestMethod -Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
+& "C:\Program Files\Teradici\PCoIP Agent\pcoip-register-host.ps1" -RegistrationCode $RegistrationCode
+rename-computer (Invoke-RestMethod-Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
 Start-Sleep 5
 Restart-Computer -Force
