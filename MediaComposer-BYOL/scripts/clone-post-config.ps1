859b8d7006e74eaa400d9a4a7386fa672dd3977e
Benjamin Ghis <benjamin.ghis@avid.com>
2020-08-12 15:16:56 -0700

    fix link to ps1 script and irm method

--- MediaComposer-BYOL/scripts/clone-post-config.ps1
+++ MediaComposer-BYOL/scripts/clone-post-config.ps1
@@ -3,6 +3,6 @@ param (
 )
 Start-Sleep 30
 & "C:\Program Files\Teradici\PCoIP Agent\pcoip-register-host.ps1" -RegistrationCode $RegistrationCode
-rename-computer (Invoke-RestMethod-Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
+rename-computer (Invoke-RestMethod -Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
 Start-Sleep 30
 Restart-Computer -Force
