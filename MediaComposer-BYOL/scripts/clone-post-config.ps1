b9290034a0b0ae4e1d3ba8585539d5fc015e5d6f
Benjamin Ghis <benjamin.ghis@avid.com>
2020-08-12 14:28:05 -0700

    increase sleep duration in ps1 script for hostname renaming

--- MediaComposer-BYOL/scripts/clone-post-config.ps1
+++ MediaComposer-BYOL/scripts/clone-post-config.ps1
@@ -1,8 +1,8 @@
 param (
     [Parameter(Mandatory=$true, ParameterSetName="Register")][string]$RegistrationCode
 )
-Start-Sleep 10
+Start-Sleep 30
 & "C:\Program Files\Teradici\PCoIP Agent\pcoip-register-host.ps1" -RegistrationCode $RegistrationCode
 rename-computer (Invoke-RestMethod-Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
-Start-Sleep 5
+Start-Sleep 30
 Restart-Computer -Force
