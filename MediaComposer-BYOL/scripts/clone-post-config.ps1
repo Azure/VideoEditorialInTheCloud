620f25594c2de91256fe4029fceec83eba41d38a
benjamin-ghis <55256429+benjamin-ghis@users.noreply.github.com>
2020-08-17 22:41:09 -0700

    Update clone-post-config.ps1

--- MediaComposer-BYOL/scripts/clone-post-config.ps1
+++ MediaComposer-BYOL/scripts/clone-post-config.ps1
@@ -2,6 +2,7 @@ param (
     [Parameter(Mandatory=$true, ParameterSetName="Register")][string]$RegistrationCode
 )
 Start-Sleep 30
+& "C:\Program Files\Teradici\PCoIP Agent\pcoip-register-host.ps1" -RegistrationCode $RegistrationCode
 rename-computer (Invoke-RestMethod -Headers @{"Metadata"="true"} -uri "http://169.254.169.254/metadata/instance?api-version=2017-08-01" -method Get).compute.name
 Start-Sleep 30
 Restart-Computer -Force
